70e05375b8d87c8171a1dace1e176bb0ce67c1ab
Thamognya Kodi <contact@thamognya.com>
2022-02-24 18:40:49 +0700

    added new patch to st

--- st-0.8.5/x.c
+++ st-0.8.5/x.c
@@ -256,6 +256,7 @@ static char *opt_name  = NULL;
 static char *opt_title = NULL;
 
 static int oldbutton = 3; /* button event on startup: 3 = release */
+static int cursorblinks = 0;
 
 void
 clipcopy(const Arg *dummy)
@@ -1563,29 +1564,50 @@ xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
 	/* draw the new one */
 	if (IS_SET(MODE_FOCUSED)) {
 		switch (win.cursor) {
-		case 7: /* st extension */
-			g.u = 0x2603; /* snowman (U+2603) */
+       default:
+       case 0: /* blinking block */
+       case 1: /* blinking block (default) */
+           if (IS_SET(MODE_BLINK))
+               break;
+
 			/* FALLTHROUGH */
-		case 0: /* Blinking Block */
-		case 1: /* Blinking Block (Default) */
-		case 2: /* Steady Block */
+           case 2: /* steady block */
 			xdrawglyph(g, cx, cy);
-			break;
-		case 3: /* Blinking Underline */
-		case 4: /* Steady Underline */
+            break;
+       case 3: /* blinking underline */
+           if (IS_SET(MODE_BLINK))
+               break;
+           /* FALLTHROUGH */
+       case 4: /* steady underline */
+
 			XftDrawRect(xw.draw, &drawcol,
 					win.hborderpx + cx * win.cw,
 					win.vborderpx + (cy + 1) * win.ch - \
 						cursorthickness,
 					win.cw, cursorthickness);
 			break;
-		case 5: /* Blinking bar */
-		case 6: /* Steady bar */
+       case 5: /* blinking bar */
+           if (IS_SET(MODE_BLINK))
+               break;
+           /* FALLTHROUGH */
+       case 6: /* steady bar */
+
 			XftDrawRect(xw.draw, &drawcol,
 					win.hborderpx + cx * win.cw,
 					win.vborderpx + cy * win.ch,
 					cursorthickness, win.ch);
 			break;
+
+       case 7: /* blinking st cursor */
+           if (IS_SET(MODE_BLINK))
+               break;
+           /* FALLTHROUGH */
+       case 8: /* steady st cursor */
+           g.u = xsetcursor;
+           xdrawglyph(g, cx, cy);
+           break;
+
+
 		}
 	} else {
 		XftDrawRect(xw.draw, &drawcol,
@@ -1742,9 +1764,12 @@ xsetmode(int set, unsigned int flags)
 int
 xsetcursor(int cursor)
 {
-	if (!BETWEEN(cursor, 0, 7)) /* 7: st extension */
+	if (!BETWEEN(cursor, 0, 8)) /* 7-8: st extensions */
 		return 1;
 	win.cursor = cursor;
+	cursorblinks = win.cursor == 0 || win.cursor == 1 ||
+	               win.cursor == 3 || win.cursor == 5 ||
+	               win.cursor == 7;
 	return 0;
 }
 
@@ -1988,6 +2013,10 @@ run(void)
 		if (FD_ISSET(ttyfd, &rfd) || xev) {
 			if (!drawing) {
 				trigger = now;
+				if (IS_SET(MODE_BLINK)) {
+					win.mode ^= MODE_BLINK;
+				}
+				lastblink = now;
 				drawing = 1;
 			}
 			timeout = (maxlatency - TIMEDIFF(now, trigger)) \
@@ -1998,7 +2027,7 @@ run(void)
 
 		/* idle detected or maxlatency exhausted -> draw */
 		timeout = -1;
-		if (blinktimeout && tattrset(ATTR_BLINK)) {
+		if (blinktimeout && (cursorblinks || tattrset(ATTR_BLINK))) {
 			timeout = blinktimeout - TIMEDIFF(now, lastblink);
 			if (timeout <= 0) {
 				if (-timeout > blinktimeout) /* start visible */
